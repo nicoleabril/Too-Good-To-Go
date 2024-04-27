559e4966c49fe5f0e67879d2b3e6bdc8fa48e1d5
JartzDz <117052958+JartzDz@users.noreply.github.com>
2024-04-26 22:24:35 -0500

    commit

--- react-toogoodtogo/src/App.js
+++ react-toogoodtogo/src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import "./assets/styles/App.css";
-
+import Cliente from "./pages/Cliente"
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route path="/Inicio" element={<Cliente />} />
         </Routes>
       </Router>
     </div>
---
+++ react-toogoodtogo/src/pages/Cliente.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const Cliente = () => {
+  return (
+    <div>
+      <h1>Página del Cliente</h1>
+      <p>Bienvenido a la página del cliente.</p>
+    </div>
+  );
+};
+
+export default Cliente;
