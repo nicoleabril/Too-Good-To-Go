cfa5e2ad0473a8489f1c32c59155dfea5cc693db
cami-vgs01 <camila.granda@ucuenca.edu.ec>
2024-05-06 09:52:56 -0500

    avanceCrud

--- react-toogoodtogo/src/assets/components/CRUD-products.js
+++ react-toogoodtogo/src/assets/components/CRUD-products.js
@@ -1,10 +1,84 @@
 import React from "react";
 import "../styles/crud-product.css";
-//import actualizar from "../images/actualizar.png";
-//import borrar from "../images/eliminar.png";
 import buscar from "../images/buscar.png";
+import DataTable from "react-data-table-component";
+import { BsTrash } from "react-icons/bs";
+import { FiEdit } from "react-icons/fi";
 
 function CRUDProducts() {
+    const columns = [
+        {
+            name: "Id",
+            selector : row => row.id,
+            sortable: true
+        },
+        {
+            name: "Producto",
+            selector : row => row.producto,
+            sortable: true
+        },
+        {
+            name: "Categoría",
+            selector : row => row.categoria,
+            sortable: true
+        },
+        {
+            name: "Precio",
+            selector : row => row.precio,
+            sortable: true
+        },
+        {
+            name: "Acciones",
+            cell: row => (
+                <div className="botonesCrud">
+                    <button>
+                        <FiEdit size={25}/>
+                    </button>
+                    <button>
+                        <BsTrash size={25}/>
+                    </button>
+                </div>
+            )
+        }
+    ];
+    const data = [
+        {
+            id: 1,
+            producto: "Combo #1",
+            categoria: "Combos",
+            precio: "$25,99",
+        },
+        {
+            id: 2,
+            producto: "Combo #2",
+            categoria: "Combos",
+            precio: "$19,99",
+        },
+        {
+            id: 3,
+            producto: "Combo #3",
+            categoria: "Combos",
+            precio: "$20,99",
+        },
+        {
+            id: 4,
+            producto: "Té Helado Grande",
+            categoria: "Bebidas",
+            precio: "$1,50",
+        },
+        {
+            id: 5,
+            producto: "Iced Latte",
+            categoria: "Bebidas",
+            precio: "$3,50",
+        },
+        {
+            id: 6,
+            producto: "Chocolate Frío",
+            categoria: "Bebidas",
+            precio: "$3,75",
+        }
+    ];
     return (
         <div className="crud-producto-container">
             <div className="crud-producto">
@@ -13,6 +87,12 @@ function CRUDProducts() {
                     <input type="text" className="TextoBusquedaProducto" placeholder="Buscar Producto" />
                 </div>
                 <button className="botonAgregarProducto">Agregar Producto</button>
+            </div>
+            <div className="tabla-productos-container">
+                <DataTable
+                    columns={columns} 
+                    data={data} 
+                    />
             </div>
         </div>
     );
