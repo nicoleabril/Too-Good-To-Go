d5b86a9d8a35a092dde69fc65be8490c88237a6f
nicoleabril <93956545+nicoleabril@users.noreply.github.com>
2024-05-08 23:10:49 -0500

    casi terminada pagina informativa

--- react-toogoodtogo/src/assets/components/Home.js
+++ react-toogoodtogo/src/assets/components/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import '../styles/informativa.css'
 //import dunkin_logo from '../images/dunkin_donuts_logo.jpeg'
 import bolsa from '../images/we-are-open.png'
@@ -8,8 +8,20 @@ import bolsa4 from '../images/bolsa4.jpg'
 import cliente from '../images/cliente.jpg'
 import negocio from '../images/negocio.jpg'
 import perfilMujer from '../images/perfilMujer.jpg'
+import dosnegocios from '../images/dosnegocios.jpg'
+import entregando from '../images/entregando.jpg'
 import ServicioCards from './ServicioCards';
 function Home() {
+  const [currentSlide, setCurrentSlide] = useState('cliente');
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide(currentSlide === 'cliente' ? 'negocio' : 'cliente');
+    }, 4000);
+
+    return () => clearInterval(interval);
+  }, [currentSlide]);
+
   const ofertasData = [
     {
       name: 'Clientes',
@@ -73,62 +85,60 @@ function Home() {
                 </div>   
                 <h1>Nuestros Servicios</h1>
                 <div className="linea"></div>     
-                <ServicioCards productos={ofertasData} nombreBoton={'SER PARTE'}/>  
+                  <ServicioCards productos={ofertasData} nombreBoton={'SER PARTE'}/>  
                 <div className="linea"></div>     
                 <div class="contenedorSlide">
-                  <div id="cliente" className="cuadroTexto">
+                  <div id="cliente" className={`cuadroTexto ${currentSlide === 'cliente' ? '' : 'oculto'}`}>
                     <div className='tituloCuadroTexto'>
                       <h1>¿Cómo funciona el proceso de compra</h1>
                       <h1 style={{ color: '#B01818' }}>para un cliente?</h1>
                     </div>
-                    <ul className="custom-list">
-                      <li>El usuario se registra como cliente en la página web para acceder a las promociones y ofertas disponibles.</li>
-                      <li>El cliente explora las promociones disponibles en la página web. La plataforma facilita el registro de la dirección, lo que le permite visualizar los restaurantes cercanos y las ofertas disponibles en cada uno de ellos.</li>
-                      <li>El cliente elige el restaurante de su agrado entre las opciones disponibles.</li>
-                      <li>En el perfil de cada restaurante, el cliente encuentra una variedad de fundas sorpresa que contienen una selección de alimentos a un precio reducido.</li>
-                      <li>El cliente selecciona la funda sorpresa que desea y reserva su pedido a través de la plataforma para que el restaurante pueda apartarlo.</li>
-                      <li>Una vez confirmada la reserva, el cliente recibe un correo electrónico de confirmación con los detalles del pedido y el horario en el cual debe recogerlo en el restaurante.</li>
-                    </ul>
+                    <div className='contenedorLista'>
+                      <ul className="custom-list">
+                        <li>El usuario se registra como cliente en la página web para acceder a las promociones y ofertas disponibles.</li>
+                        <li>El cliente explora las promociones disponibles en la página web. La plataforma facilita el registro de la dirección, lo que le permite visualizar los restaurantes cercanos y las ofertas disponibles en cada uno de ellos.</li>
+                        <li>El cliente elige el restaurante de su agrado entre las opciones disponibles.</li>
+                        <li>En el perfil de cada restaurante, el cliente encuentra una variedad de fundas sorpresa que contienen una selección de alimentos a un precio reducido.</li>
+                        <li>El cliente selecciona la funda sorpresa que desea y reserva su pedido a través de la plataforma para que el restaurante pueda apartarlo.</li>
+                        <li>Una vez confirmada la reserva, el cliente recibe un correo electrónico de confirmación con los detalles del pedido y el horario en el cual debe recogerlo en el restaurante.</li>
+                      </ul>
+                      <img src={entregando} alt="Entregando" className="imagenProceso" />
+                    </div>
                   </div>
-                  <div id="negocio" className="cuadroTexto oculto">
+                  <div id="negocio" className={`cuadroTexto ${currentSlide === 'negocio' ? '' : 'oculto'}`}>
                     <div className='tituloCuadroTexto'>
                       <h1>¿Cómo funciona el proceso de reserva</h1>
                       <h1 style={{ color: '#B01818' }}>para un negocio?</h1>
                     </div>
-                    <ul className="custom-list">
-                      <li>Regístrate como negocio proporcionando la información requerida.</li>
-                      <li>Agrega los productos disponibles en tu establecimiento a través de un formulario en la plataforma.</li>
-                      <li>Utiliza la función de gestión de categorías para organizar tus productos de manera efectiva y facilitar la navegación de los clientes.</li>
-                      <li>Crea ofertas especiales para atraer a los clientes, proporcionando detalles sobre la disponibilidad y el precio reducido.</li>
-                      <li>Una vez que los clientes reserven sus pedidos a través de la plataforma, recibirás una notificación y podrás preparar los productos para su recogida.</li>
-                      <li>Confirma cada pedido realizado por los clientes a través de la plataforma y proporciona información sobre el horario de recogida.</li>
-                    </ul>
+                    <div  className='contenedorLista'>
+                      <ul className="custom-list">
+                        <li>Regístrate como negocio proporcionando la información requerida.</li>
+                        <li>Agrega los productos disponibles en tu establecimiento a través de un formulario en la plataforma.</li>
+                        <li>Utiliza la función de gestión de categorías para organizar tus productos de manera efectiva y facilitar la navegación de los clientes.</li>
+                        <li>Crea ofertas especiales para atraer a los clientes, proporcionando detalles sobre la disponibilidad y el precio reducido.</li>
+                        <li>Una vez que los clientes reserven sus pedidos a través de la plataforma, recibirás una notificación y podrás preparar los productos para su recogida.</li>
+                        <li>Confirma cada pedido realizado por los clientes a través de la plataforma y proporciona información sobre el horario de recogida.</li>
+                      </ul>
+                      <img src={dosnegocios} alt="Negocios" className="imagenProceso" />
+                    </div>
                   </div>
                   <div className="controles">
-                    <input type="radio" id="cliente-dot" name="dot" className="dot-radio" checked />
-                    <label htmlFor="cliente-dot" className="dot" onClick={() => document.getElementById("cliente-dot").click()}></label>
-                    <input type="radio" id="negocio-dot" name="dot" className="dot-radio" />
-                    <label htmlFor="negocio-dot" className="dot" onClick={() => document.getElementById("negocio-dot").click()}></label>
+                    <input type="radio" id="cliente-dot" name="dot" className="dot-radio" checked={currentSlide === 'cliente'} />
+                    <label htmlFor="cliente-dot" className="dot" onClick={() => setCurrentSlide('cliente')}></label>
+                    <input type="radio" id="negocio-dot" name="dot" className="dot-radio" checked={currentSlide === 'negocio'} />
+                    <label htmlFor="negocio-dot" className="dot" onClick={() => setCurrentSlide('negocio')}></label>
                   </div>
                 </div>
                 <h1>Recoge tu Pedido</h1>
                 <div className="linea"></div>
-                <div className='seccionIniciativa'>
-                  <div className='seccionMensaje'>
-                    <div className='seccionImagen'>
-                      <img src={bolsa2} alt="Bolsa 1" className="imagenSecundaria" />
-                      <img src={bolsa3} alt="Bolsa 2" className="imagenPrimaria" />
-                      <img src={bolsa4} alt="Bolsa 3" className="imagenSecundaria" />
-                    </div>
-                    <div className='texto-seccionImagen'>
-                      <p >¡Tú puedes marcar la diferencia!</p>
-                    </div>
-                  </div>
-                  <div className='seccionTexto'>
-                    <h1>INICIATIVA</h1>
-                    <div className='textoIniciativa'>
-                      <p>En nuestros restaurantes locales, demasiada comida termina en la basura todos los días. Pero creemos que podemos cambiar eso.Para los restaurantes, reducir el desperdicio de alimentos no solo significa ahorrar dinero y mejorar la eficiencia operativa.Para nuestros clientes, participar en Too Good To Go significa disfrutar de deliciosas ofertas a precios reducidos mientras contribuyen a un mundo más sostenible.</p>
-                    </div>
+                <div className='seccionPedido'>
+                  <div id="reservaConfirmada">
+                    <h1 style={{ color: '#B01818' }}>¿Qué hacer una vez que el restaurante a confirmado la reserva?</h1>
+                    <ul>
+                      <li>Después de confirmar tu reserva, recibirás un correo electrónico de confirmación para mantenerte al tanto de tu pedido y con el horario en que puedes recogerlo.</li>
+                      <li>Es importante recoger tu pedido dentro del horario indicado para evitar que se cancele automáticamente.</li>
+                      <li>Después de recoger tu pedido, podrás calificar el producto y dejar comentarios para compartir tu experiencia con otros usuarios.</li>
+                    </ul>
                   </div>
                 </div>  
             </div>
--- react-toogoodtogo/src/pages/Login.js
+++ react-toogoodtogo/src/pages/Login.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
-import '../assets/styles/login.css'
+import '../assets/styles/login.css';
 import { FaCircleUser } from "react-icons/fa6";
 import { RiLockPasswordFill } from "react-icons/ri";
 import burger from '../assets/images/burger.png';
+import Cookies from 'js-cookie';
 
 export default class Login extends Component {
   constructor(props) {
@@ -10,22 +11,89 @@ export default class Login extends Component {
     this.state = {
       username: '',
       password: '',
+      rol: '',
+      token: Cookies.get('authToken'),
+      isLoggedIn: false,
+      currentImageIndex: 0,
+      error: false,
     };
   }
-  
+
+  userData = [
+    {
+      username: "nico@correo.com",
+      password: "nico123",
+      rol: "admin"
+    },
+    {
+      username: "cami@correo.com",
+      password: "cami123",
+      rol: "cliente"
+    },
+    {
+      username: "emi@correo.com",
+      password: "emi123",
+      rol: "negocio"
+    },
+    // Agrega más usuarios si lo deseas
+  ];
+
+  handleError = () => {
+    this.setState({ error: true });
+  }
+
+  handleInputChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  }
+
+  handleEnterPress = (e) => {
+    if (e.key === 'Enter') {
+      this.handleSubmit(e);
+    }
+  }
+
   handleSubmit = (e) => {
     e.preventDefault();
-    // Aquí puedes manejar la lógica de envío del formulario
-    console.log('Formulario enviado');
+    const { username, password } = this.state;
+    const user = this.userData.find(user => user.username === username && user.password === password);
+
+    if (user) {
+      // Si el usuario existe, establecer el estado correspondiente
+      this.setState({
+        rol: user.rol,
+        isLoggedIn: true,
+        error: false,
+        token: 'mockToken123' // Podrías generar un token aquí si lo necesitas
+      });
+
+      // Simular almacenamiento del token en cookies
+      Cookies.set('authToken', 'mockToken123');
+      Cookies.set('usr', username);
+      Cookies.set('rol', user.rol);
+
+      // Redirigir según el rol después de la autenticación
+      if (user.rol === 'cliente') {
+        window.location.href = '/Inicio';
+      } else if (user.rol === 'negocio') {
+        window.location.href = '/Restaurante';
+      } else {
+        window.location.href = '/Inicio'; // Redirección por defecto
+      }
+    } else {
+      // Si el usuario no existe, mostrar error
+      this.setState({
+        error: true
+      });
+    }
   }
 
   render() {
     return (
-     
       <div className="container">
         <div className="waves-background"></div>
         <div className="loginForm">
-          <form onSubmit={this.handleSubmit}>
+          <form>
             <h1 className="too-good">Too Good</h1>
             <h1 className="to-go">To Go</h1>
             <div className="input-box">
@@ -33,8 +101,8 @@ export default class Login extends Component {
                 type="email"
                 placeholder="Correo Electrónico"
                 required
-                value={this.state.username}
-                onChange={(e) => this.setState({ username: e.target.value })}
+                name="username"
+                onChange={this.handleInputChange}
               />
               <FaCircleUser className="icon" />
             </div>
@@ -43,15 +111,16 @@ export default class Login extends Component {
                 type="password"
                 placeholder="Contraseña"
                 required
-                value={this.state.password}
-                onChange={(e) => this.setState({ password: e.target.value })}
+                name="password"
+                onChange={this.handleInputChange}
+                onKeyDown={this.handleEnterPress}
               />
               <RiLockPasswordFill className="icon" />
             </div>
             <div className="forgot-password">
               <a href="#">¿Olvidaste la Contraseña?</a>
             </div>
-            <button type="submit">Iniciar Sesión</button>
+            <button onClick={this.handleSubmit}>Iniciar Sesión</button>
             <div className="new-account">
               <p>¿Eres nuevo aquí? <a href="#">Crear cuenta</a></p>
             </div>
@@ -66,7 +135,6 @@ export default class Login extends Component {
           </div>
         </div>
       </div>
-     
     );
   }
 }
