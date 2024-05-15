04a33ec52b750bbe2035a86187cd6f2cdcc033b8
JartzDz <117052958+JartzDz@users.noreply.github.com>
2024-05-15 10:05:29 -0500

    update cliente.css

--- react-toogoodtogo/src/assets/components/HomeCliente.js
+++ react-toogoodtogo/src/assets/components/HomeCliente.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
-import pizza from '../images/pizza.png'
+import pizza from '../images/pizza.png';
 import LocalesCards from './LocalesCards';
-import McDonalds from '../images/McDonalds.png'
-import KFC from '../images/KFC.png'
-import BurgerKing from '../images/BurgerKing.png'
-import '../styles/cliente.css'
+import McDonalds from '../images/McDonalds.png';
+import KFC from '../images/KFC.png';
+import BurgerKing from '../images/BurgerKing.png';
+import MapComponent from './MapComponent'; 
+import '../styles/cliente.css';
+
 function HomeCliente() {
   const localesData = [
     {
@@ -33,44 +34,37 @@ function HomeCliente() {
       menu: ['Burgers', 'Papas fritas', 'Malteadas']
     }
   ];
-    
-    return (
-    
-        <div>
-            <div className='textoImagen'>
-            <h1 className='texto1'>Tu comida favorita</h1>
-            <h1 className='texto2'>al mejor precio</h1>
-            <p className='subtexto'>Revisa todas las ofertas. ¡Seguro te encantarán!</p>
-            </div>
-            <div className="imagenPizza">
-            <img src={pizza} alt="Pizza" className="imagen2" />
-            </div>
-            <div className="waves-background2"></div>
-            <div className="contenedorRojo">
-            </div>
-            <div className="contenedorBlanco">
-                    <h1>OFERTAS</h1>
-                    <div className="linea"></div>
-                    <LocalesCards locales={localesData} nombreBoton={'VER OFERTAS'} />
-                    <h1>Negocios</h1>
-                    <LocalesCards locales={localesData} nombreBoton={'COMPRAR AHORA'}/>
-                    <h1>¿Buscas de lo siempre?</h1>
-                    <LocalesCards locales={localesData} nombreBoton={'COMPRAR AHORA'}/>
-                    <h1>Localización</h1>
-                   
-                   
-            </div>
-            <div className="contenedorFooter">
-              <div className="textoFooter2">
-                Copyright © 2024 Too Good To Go International. All Rights Reserved.
-              </div>
-            </div>
-        </div>
-
-    
-    );
-    
-    
-  }
   
-  export default HomeCliente;
+  return (
+    <div>
+      <div className='textoImagen'>
+        <h1 className='texto1'>Tu comida favorita</h1>
+        <h1 className='texto2'>al mejor precio</h1>
+        <p className='subtexto'>Revisa todas las ofertas. ¡Seguro te encantarán!</p>
+      </div>
+      <div className="imagenPizza">
+        <img src={pizza} alt="Pizza" className="imagen2" />
+      </div>
+      <div className="waves-background2"></div>
+      <div className="contenedorRojo"></div>
+      <div className="contenedorBlanco">
+        <h1>OFERTAS</h1>
+        <div className="linea"></div>
+        <LocalesCards locales={localesData} nombreBoton={'VER OFERTAS'} />
+        <h1>Negocios</h1>
+        <LocalesCards locales={localesData} nombreBoton={'COMPRAR AHORA'} />
+        <h1>¿Buscas de lo siempre?</h1>
+        <LocalesCards locales={localesData} nombreBoton={'COMPRAR AHORA'} />
+        <h1>Localización</h1>
+        
+      </div>
+      <div className="contenedorFooter">
+        <div className="textoFooter2">
+          Copyright © 2024 Too Good To Go International. All Rights Reserved.
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HomeCliente;
---
+++ react-toogoodtogo/src/assets/components/MapComponent.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+const MapComponent = () => {
+  return (
+    <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: "400px", width: "100%" }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          Aquí puedes poner información sobre este marcador.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default MapComponent;
