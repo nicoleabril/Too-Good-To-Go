aba2120419c2908938f650f5cdd3a5862e78f3f1
JartzDz <117052958+JartzDz@users.noreply.github.com>
2024-05-19 18:20:11 -0500

    responsive Inicio-Negocio

--- react-toogoodtogo/src/pages/InicioNegocio.js
+++ react-toogoodtogo/src/pages/InicioNegocio.js
@@ -3,7 +3,6 @@ import React from 'react';
 import Header from '../assets/components/Header';
 import DynamicBreadcrumb from '../assets/components/Bredcrumb';
 import HomeNegocio from '../assets/components/HomeNegocio';
-import '../assets/styles/cliente.css'
 import Chatbot from '../assets/components/Chatbot';
 const NegocioPage = () => {
   return (
