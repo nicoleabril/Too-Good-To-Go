d5f1e6ed6ffb9240f0b42c1593a81ab7c4b85bea
Emily Romero <romero.emily.225@gmail.com>
2024-05-19 10:06:39 -0500

    Se agrego el boton y el collage funciona completamente

--- react-toogoodtogo/src/assets/components/Reserva/ResumenDelaReserva.js
+++ react-toogoodtogo/src/assets/components/Reserva/ResumenDelaReserva.js
@@ -1,31 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Collapsible from 'react-collapsible';
 import ProductoReservado from './ProductoReservado';
 import datosProductosComprados from '../../components/Reserva/datosProductosComprados';
 import '../../styles/paginadeReserva.css';
+import { FaAngleDown } from "react-icons/fa6";
+import { FaAngleUp } from "react-icons/fa6";
+
 
 
 
 function ResumenDelaReserva(props) {
     let cantidad = 1;
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleTriggerClick = () => {
+        setIsOpen(!isOpen);
+    };
     return (
         <>
-       
-        <Collapsible trigger="Resumen de los pedidos" className='tittleToggleSection'   >
-            
-        {/*Vamos a crear un componente por cada producto que se encuentre en datosProductosComprados */}
-        {datosProductosComprados.products.map((producto,index)=>{
-            return <ProductoReservado
-            key={index} // Agrega una clave única para cada componente en el mapeo
-            imgProducto={producto.imgProducto}
-            nombreProducto={producto.nombrePromocion}
-            descripcionProducto={producto.descripcion}
-            precioProducto={producto.precio}
-            cantidad={cantidad} // Pasa la cantidad como un prop
-        />;
-        })}
-
-        </Collapsible>
+            <Collapsible
+                trigger={
+                    <div className="triggerContent">
+                        <span>Resumen de los pedidos</span>
+                        <button className="triggerButton" onClick={handleTriggerClick}>
+                            {isOpen ? <FaAngleUp /> : <FaAngleDown />}
+                        </button>
+                    </div>
+                }
+                className="tittleToggleSection"
+                open={isOpen}
+                onOpening={() => setIsOpen(true)}
+                onClosing={() => setIsOpen(false)}
+            >
+                {/* Crear un componente por cada producto en datosProductosComprados */}
+                {datosProductosComprados.products.map((producto, index) => (
+                    <ProductoReservado
+                        key={index} // Agrega una clave única para cada componente en el mapeo
+                        imgProducto={producto.imgProducto}
+                        nombreProducto={producto.nombrePromocion}
+                        descripcionProducto={producto.descripcion}
+                        precioProducto={producto.precio}
+                        cantidad={1} // Pasa la cantidad como un prop
+                    />
+                ))}
+            </Collapsible>
         </>
     );
 }
