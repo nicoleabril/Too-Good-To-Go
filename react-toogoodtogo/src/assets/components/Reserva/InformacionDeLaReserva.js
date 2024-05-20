40432f0d4775e06400643ab461086f251be2db87
Emily Romero <romero.emily.225@gmail.com>
2024-05-19 20:43:10 -0500

    un poquito de avance

--- react-toogoodtogo/src/assets/components/Reserva/InformacionDeLaReserva.js
+++ react-toogoodtogo/src/assets/components/Reserva/InformacionDeLaReserva.js
@@ -1,15 +1,23 @@
 import React from 'react';
 
-function InformacionDeLaReserva(props) {
+function InformacionDeLaReserva({metodoPago, estado, restaurante, direccion, horario}) {
     return (
         <>
         <h3 className='texto_DetallesReserva'>Detalles de la reserva</h3>
-        <p className='texto_InformacionReserva'>Estado:</p>
+        <div className='cont-DetallesDePago'>
+        
         <p className='texto_InformacionReserva'>Método de Pago:</p>
+        <label className='label_InformacionReserva'>{metodoPago}</label>
+        <p className='texto_InformacionReserva'>Estado:</p>
+        <label className='label_InformacionReserva'>{estado}</label>
         <p className='texto_InformacionReserva'>Restaurante:</p>
+        <label className='label_InformacionReserva'>{restaurante}</label>
         <p className='texto_InformacionReserva'>Dirección:</p>
-        <p className='texto_InformacionReserva'>Horario:</p>        
+        <label className='label_InformacionReserva'>{direccion}</label>
+        <p className='texto_InformacionReserva'>Horario:</p>
+        <label className='label_InformacionReserva'>{horario}</label>        
 
+        </div>
         </>
     );
 }
--- react-toogoodtogo/src/pages/PaginaDeReserva.js
+++ react-toogoodtogo/src/pages/PaginaDeReserva.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../assets/styles/paginadeReserva.css';
 import '../assets/components/Reserva/CabeceraDelResumen';
 import CabeceraDelResumen from '../assets/components/Reserva/CabeceraDelResumen';
@@ -6,6 +6,18 @@ import ResumenDelaReserva from '../assets/components/Reserva/ResumenDelaReserva'
 import datosProductosComprados from '../assets/components/Reserva/datosProductosComprados';
 import IngresoDatosPersonales from '../assets/components/Reserva/IngresoDatosPersonales';
 import InformacionDeLaReserva from '../assets/components/Reserva/InformacionDeLaReserva';
+
+
+const dataInformacionReserva = [
+  {
+    metodoPago: 'Tarjeta de Crédito',
+    estado: 'Pendiente',
+    restaurante: 'Restaurante de Prueba',
+    direccion: 'Calle de Prueba',
+    horario: '12:00 - 14:00'
+  }
+
+];
 
 /* EN TEORÍA AQUÍ SE PASAN TODOS LOS DATOS DEL RESTAURANTE */
 function PaginaDeReserva() {
@@ -25,32 +37,32 @@ function PaginaDeReserva() {
     const nuevosProductos = [...productos];
     nuevosProductos[index].cantidadVendida += 1;
     setProductos(nuevosProductos);
-};
-/* Decrementa la cantidad de un producto */
-const decrementarCantidad = (index) => {
+  };
+  /* Decrementa la cantidad de un producto */
+  const decrementarCantidad = (index) => {
     const nuevosProductos = [...productos];
     if (nuevosProductos[index].cantidadVendida > 0) {
-        nuevosProductos[index].cantidadVendida -= 1;
-        setProductos(nuevosProductos);
+      nuevosProductos[index].cantidadVendida -= 1;
+      setProductos(nuevosProductos);
     }
-};
+  };
 
   return (
     <div className='cont-principalReserva' >
-
+      <header page={'Reserva'} />
       <div className='cont-resumenReserva'>
         <CabeceraDelResumen />
-        <ResumenDelaReserva 
-        productos={productos}
-        onIncrement={incrementarCantidad}
-        onDecrement={decrementarCantidad}
+        <ResumenDelaReserva
+          productos={productos}
+          onIncrement={incrementarCantidad}
+          onDecrement={decrementarCantidad}
         />
         <p className='totalReserva'>Total: $ {total}</p>
       </div>
       <div className='cont-detallesReserva'>
         <h1 className='texto_CompletaReserva'>Completa tu reserva</h1>
         <IngresoDatosPersonales />
-        <InformacionDeLaReserva />  
+        <InformacionDeLaReserva metodoPago={dataInformacionReserva[0].metodoPago} estado={dataInformacionReserva[0].estado} restaurante={dataInformacionReserva[0].restaurante} direccion={dataInformacionReserva[0].direccion} horario={dataInformacionReserva[0].horario} />
       </div>
 
     </div>
