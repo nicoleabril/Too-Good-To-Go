03fbf792bd7eb542d9e5e8af6450b3587d1ae040
nicoleabril <nicole.abrilc@ucuenca.edu.ec>
2024-07-20 18:31:35 -0500

    Seccion de comentario terminada

--- react-toogoodtogo/src/assets/components/ComentarioCard.js
+++ react-toogoodtogo/src/assets/components/ComentarioCard.js
@@ -4,14 +4,16 @@ import ensalada from '../images/ensalada.png'
 
 const ComentarioCard = () => {
   return (
-    <div className="contact-form-container">
-      <img src={ensalada} alt="Salad" className="salad-image" />
-      <div className="form-section">
-        <h2>¿CÓMO HA ESTADO LA COMIDA?</h2>
-        <p>Envíanos un correo y lo tendremos en cuenta lo antes posible</p>
-        <textarea placeholder="Escribe aquí tu duda." className="text-area"></textarea>
-        <button className="submit-button">Enviar</button>
-      </div>
+    <div className="contact-form-wrapper">
+        <div className="contact-form-container">
+            <img src={ensalada} alt="Salad" className="salad-image" />
+            <div className="form-section">
+                <h2>¿CÓMO HA ESTADO LA COMIDA?</h2>
+                <p>Envíanos un correo y lo tendremos en cuenta lo antes posible</p>
+                <textarea placeholder="Escribe aquí tu experiencia." className="text-area"></textarea>
+                <button className="submit-button">Enviar</button>
+            </div>
+        </div>
     </div>
   );
 };
